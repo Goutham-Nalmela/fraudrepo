d81851666a3f4dc7df0750c007acad50bdc4e5bc
workbench <>
2016-09-23 14:30:17 +0530

    {/FraudManagement/src/main/java/com/radial/fraudmanagement/PersonName.java}

--- FraudManagement/src/main/java/com/radial/fraudmanagement/PersonName.java
+++ FraudManagement/src/main/java/com/radial/fraudmanagement/PersonName.java
@@ -9,8 +9,8 @@ public class PersonName implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
-   private java.lang.String givenName;
-   private java.lang.String surname;
+   public java.lang.String givenName;
+   public java.lang.String surname;
 
    public PersonName()
    {
